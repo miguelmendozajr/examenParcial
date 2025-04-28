51fefbaea8e03c961a9075be914fbec9e86e83b5
miguelmendozajr <miguelmendozaj20@gmail.com>
2025-04-28 12:46:35 -0600

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const studentsRoutes = require('./src/routes/student');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api/students', studentsRoutes);
